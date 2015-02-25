2c17d7534b9739a38d294d191370bbd851e433af
icerunemage1 <acaoimsa@gmail.com>
2015-02-25 08:44:19 -0600

    genetic algorithm v1 up

---
+++ src/tspProject/GeneticTSP.java
@@ -0,0 +1,71 @@
+package tspProject;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class GeneticTSP {
+	
+	private static ArrayList<Vector> list = new ArrayList<Vector>();
+	private static ArrayList<ArrayList<Vector>> paths = new ArrayList<ArrayList<Vector>>();
+	
+	public static void main(String[] args) {
+		int initPaths = 1000;
+		int iterations = 10000;
+		for(int i = 0; i < initPaths; i++) {
+			addRandomPath(list.get(0));
+		}
+		for(int i = 0; i < iterations; i++) {
+			mutation();
+			cull();
+		}
+	}
+	
+	public static void addRandomPath(Vector start) {
+		ArrayList<Vector> echo = list;
+		ArrayList<Vector> path = new ArrayList<Vector>();
+		echo.remove(start);
+		Collections.shuffle(echo);
+		echo.add(0, start);
+		echo.add(list.size(), start);
+		paths.add(echo);
+	}
+	
+	public static void mutation() {
+		Random random = new Random();
+		int rand = random.nextInt(paths.size());
+		ArrayList<Vector> mutated= paths.get(rand);
+		int mutPoint1 = 0;
+		int mutPoint2 = 0;
+		while(mutPoint1 == mutPoint2) {
+			mutPoint1 = random.nextInt(mutated.size());
+			mutPoint2 = random.nextInt(mutated.size());
+		}
+		Vector mut1 = mutated.get(mutPoint1);
+		Vector mut2 = mutated.get(mutPoint2);
+		mutated.set(mutPoint1, mut2);
+		mutated.set(mutPoint2, mut1);
+		paths.add(mutated);
+	}
+	
+	public static void cull() {
+		ArrayList<Vector> weak = paths.get(0);
+		double length = length(weak);
+		for(ArrayList<Vector> path: paths) {
+			if(length < length(path)) {
+				weak = path;
+				length = length(path);
+			}
+		}
+		paths.remove(weak);
+	}
+	
+	public static double length(ArrayList<Vector> path) {
+		path.add(path.get(0));
+		double length = 0;
+		for(int i = 1; i < path.size(); i++) {
+			length += path.get(i).subtract(path.get(i - 1)).magnitude();
+		}
+		return length;
+	}
+}
